3e8c500f68c5297e4b8557d35ae6f60a298b0a3a
Antonio Feregrino <antonio.feregrino@gmail.com>
2019-07-27 09:12:13 +0100

    Train test split

--- src/tools/utils.py
+++ src/tools/utils.py
@@ -1,6 +1,20 @@
-def relative2absolute(cx, cy, w, h, img_w, img_h):
+def relative2absolute(cx, cy, w, h, img_w, img_h, dtype='int'):
     pcx = (cx - w/2) * img_w
     pcy = (cy - h/2) * img_h
     width = w * img_w
     height = h * img_h
+    if dtype == 'int':
+        return int(pcx), int(pcy), int(width), int(height)
     return pcx, pcy, width, height
+
+
+item_template = """item {{
+    id: {class_id}
+    name: '{name}'
+}}
+"""
+
+def generate_label_map(labels):
+    return '\n'.join([
+        item_template.format(class_id=int(k), name=v) for k, v in labels.items()
+    ])
